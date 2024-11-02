ea44192ca071cde3e5cd5e316dbad7f2217a3ca5
malilalilaaa <shafwatmahiya1210@gmail.com>
2024-11-03 00:22:49 +0600

    1st commit

---
+++ week 1/netflix_ui/lib/utils/pallete.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: constant_identifier_names, use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color RedLight1 = Color(0xFFDF7375);
+  static const Color Red = Color(0xFFD22F26);
+  static const Color RedDark1 = Color(0xFFB1060F);
+  static const Color Grey1 = Color(0xFF737373);
+  static const Color GreyDark1 = Color(0xFFD323232);
+  static const Color GreyDark2 = Color(0xFFD191919);
+  static const Color GreyDark3 = Color(0xFF010101);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color GreyLight1 = Color(0xFFE6E6E6E);
+  static const Color GreyLight2 = Color(0xFFCBCBCB);
+  static const Color GreyLight3 = Color(0xFFB2B2B2);
+  static const Color SystemBlue = Color(0xFF4B6AEA);
+  static const Color SystemRed = Color(0xFFFE0202);
+  static const Color Black = Color(0xFF000000);
+}
